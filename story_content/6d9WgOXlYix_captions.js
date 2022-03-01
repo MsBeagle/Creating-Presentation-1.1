2b21549696d872da8d832cc2b0a83e49eeb4cb18
MsBeagle <aga.ihnatowicz@gmail.com>
2022-03-01 18:21:33 +0100

    training

---
+++ story_content/6d9WgOXlYix_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.44.23192.0%0D%0A%0D%0A00:00:00.085%20--%3E%2000:00:01.320%0D%0APresentation%20Templates.%0D%0A%0D%0A"}')
